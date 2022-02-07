ece26b4671d7344a2ec3ca17c54831c9d2348b70
chayanisas <98453852+chayanisas@users.noreply.github.com>
2022-02-07 16:29:48 +0700

    Delete Problem  7.sql

--- IBM_Data_Analyst/SQL/Problem  7.sql
+++
@@ -1,7 +0,0 @@
-
--- List 5 community areas with highest % of households below poverty line.--
-SELECT DISTINCT TOP 5 pub.[Community Area Name], cen.[PERCENT HOUSEHOLDS BELOW POVERTY]
-  
-  FROM [IBM].[dbo].[CHICAGO_PUBLIC_SCHOOLS] pub RIGHT JOIN [IBM].[dbo].[CENSUS_DATA] cen ON pub.[Community Area Number] = cen.[Community Area Number] 
-  
-  ORDER BY cen.[PERCENT HOUSEHOLDS BELOW POVERTY] DESC
