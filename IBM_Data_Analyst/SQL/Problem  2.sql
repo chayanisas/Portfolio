94d0f2912abcd0c1e50b9ace2e780df9d7f58e65
chayanisas <98453852+chayanisas@users.noreply.github.com>
2022-02-07 16:29:37 +0700

    Delete Problem  2.sql

--- IBM_Data_Analyst/SQL/Problem  2.sql
+++
@@ -1,5 +0,0 @@
-
--- List community areas with per capita income less than 11000. --
-SELECT [COMMUNITY AREA NAME], [PER CAPITA INCOME ]
-  FROM [IBM].[dbo].[CENSUS_DATA]
-  WHERE [PER CAPITA INCOME] < 11000
