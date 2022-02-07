44c0f73e31863ebd8c573b1cfc18b0c0edac116e
chayanisas <98453852+chayanisas@users.noreply.github.com>
2022-02-07 16:29:54 +0700

    Delete Problem  9.sql

--- IBM_Data_Analyst/SQL/Problem  9.sql
+++
@@ -1,5 +0,0 @@
-
--- Use a sub-query to find the name of the community area with highest hardship index --
-SELECT [COMMUNITY AREA NAME]
-FROM [IBM].[dbo].[CENSUS_DATA]
-WHERE [HARDSHIP INDEX] = ( SELECT MAX([HARDSHIP INDEX]) FROM [IBM].[dbo].[CENSUS_DATA])
