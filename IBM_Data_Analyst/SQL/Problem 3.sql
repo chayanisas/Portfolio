4d10c2f9899d9214895bcfe8ae23823309e8c77b
chayanisas <98453852+chayanisas@users.noreply.github.com>
2022-02-07 16:30:06 +0700

    Delete Problem 3.sql

--- IBM_Data_Analyst/SQL/Problem 3.sql
+++
@@ -1,6 +0,0 @@
-
---List all case numbers for crimes involving minors (children are not considered minors for the purposes of crime analysis)--
-
-SELECT DISTINCT [Case Number], [Description]
-FROM [IBM].[dbo].[CHICAGO_CRIME_DATA]
-WHERE [Description] LIKE '%MINOR%'
